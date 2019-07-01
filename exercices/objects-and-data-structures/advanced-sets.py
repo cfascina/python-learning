7badde0e2fc73b1cbb9152cc968e1dece1279b2f
Caio Fascina <cfascina@gmail.com>
2019-07-01 12:33:22 -0300

    Advanced sets - Part 2

--- exercices/objects-and-data-structures/advanced-sets.py
+++ exercices/objects-and-data-structures/advanced-sets.py
@@ -53,3 +53,39 @@ print("set_1: " + str(set_1))
 set_1.intersection_update(set_2)
 print("set_1.intersection_update(set_2)")
 print("set_1: " + str(set_1))
+
+print("\nIs Disjoint")
+set_1 = {1, 2}
+set_2 = {1, 2, 3, 4}
+set_3 = {3, 4}
+print("set_1: " + str(set_1))
+print("set_2: " + str(set_2))
+print("set_3: " + str(set_3))
+print("set_1.isdisjoint(set_2): " + str(set_1.isdisjoint(set_2)))
+print("set_1.isdisjoint(set_3): " + str(set_1.isdisjoint(set_3)))
+
+print("\nIs Subset and Is Superset")
+set_1 = {1, 2}
+set_2 = {1, 2, 3}
+set_3 = {2, 3, 4}
+print("set_1: " + str(set_1))
+print("set_2: " + str(set_2))
+print("set_3: " + str(set_3))
+print("set_1.issubset(set_2): " + str(set_1.issubset(set_2)))
+print("set_1.issubset(set_3): " + str(set_1.issubset(set_3)))
+print("set_2.issuperset(set_1): " + str(set_2.issuperset(set_1)))
+print("set_2.issuperset(set_3): " + str(set_2.issuperset(set_3)))
+
+print("\nSymmetric Difference, Union and Update")
+set_1 = {1, 2}
+set_2 = {1, 2, 3}
+print("set_1: " + str(set_1))
+print("set_2: " + str(set_2))
+print("set_1.symmetric_difference(set_2): " + str(set_1.symmetric_difference(set_2)))
+print("set_1.union(set_2): " + str(set_1.union(set_2)))
+print("set_1: " + str(set_1))
+print("set_2: " + str(set_2))
+set_1.update(set_2)
+print("set_1.update(set_2)")
+print("set_1: " + str(set_1))
+print("set_2: " + str(set_2))
