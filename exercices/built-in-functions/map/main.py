558ae3a3449ec87525b2936d1004d0d1c96f5c77
Caio Fascina <cfascina@gmail.com>
2019-07-08 10:45:30 -0300

    Map exercise w/ 2 iterables

--- exercices/built-in-functions/map/main.py
+++ exercices/built-in-functions/map/main.py
@@ -2,15 +2,30 @@
 # be iterade as the second parameter. Using it inside a for loop, the elements
 # inside the object will be automatically iterade within the function.
 
+# map() takes 2 or more arguments: a function and one or more iterables,
+# in the form:
+# map(function, iterable, ...)
+
+# map() returns an iterator - that is, map() returns a special object where the
+# iterables passed automatically by the the function.
+
 def square(number):
     return number ** 2
 
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3]
 
+print("Function map() results:")
 for number in map(square, numbers):
     print(number)
 
-# Using a for loop, to obtain the same results reached w/ the map function, the
-# code would be like the following:
-# for number in numbers:
-    # print(square(number))
+# map() can accept more than one iterable, since they have the same length.
+# In the following example, we added the elements of the first list w/ the
+# elements of the second list, using a lambda expression instead of a
+# previously defined function:
+
+list_1 = [1, 2, 3]
+list_2 = [4, 5, 6]
+
+print("\nFunction map() w/ two iterables results:")
+for number in map(lambda x, y: x + y, list_1, list_2):
+    print(number)
